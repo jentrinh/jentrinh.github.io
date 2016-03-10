63abecca493dc1e2476a1c9462a2da96dbdbada9
Christian Gass <christiangass@outlook.com>
2016-03-10 10:55:55 -0500

    initial commit

---
+++ agency/js/custom.js
@@ -0,0 +1,12 @@
+/* ==============================================
+ main flex slider
+ =============================================== */
+$(window).load(function() {
+    $('.main-flex-slider').flexslider({
+        slideshowSpeed: 2500,
+        directionNav: false,
+        animation: "slide",
+        controlNav: false,
+        direction: "vertical"
+    });
+});
