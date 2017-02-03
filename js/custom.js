9735d9802eea0390fcfb51b141cdf6e4374b6df8
Christian Gass <christiangass@outlook.com>
2017-02-03 14:42:32 -0500

    clean up unused file references

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,6 @@
-/* ==============================================
- main flex slider
- =============================================== */
-$(window).load(function() {
-    $('.main-flex-slider').flexslider({
-        slideshowSpeed: 2500,
-        directionNav: false,
-        animation: "slide",
-        controlNav: false,
-        direction: "vertical"
-    });
-});
-
-
-/* video background ***********************
+/**
+ * video background
  */
-/*jQuery(function() {
+jQuery(function() {
     jQuery(".player").mb_YTPlayer();
-});*/
-
-$('#header-bg-vid').vide('media/vid/trashbot/trashbot');
+});
